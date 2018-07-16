0af58d64c9a3c198752bced9d22dd07595087f34
João Marujo <jppmarujo@gmail.com>
2018-07-16 18:33:29 +0000

    Followed CS50x code styling rules.

--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -6,7 +6,7 @@
 int main(int argc, string argv[])
 {
     // check if there are 2 arguments.
-    if(argc != 2)
+    if (argc != 2)
     {
         printf("Use (only) one argument after ./vigenere\n");
         return 1;
@@ -33,44 +33,44 @@ int main(int argc, string argv[])
 
     for (int a = 0, b = strlen(text), c = 0; a < b; a++)
     {
-                // checks if the char[n] in string 'text' is alphabetical.
-                if (isalpha(text[a]))
-                {
-                    // checks if the char[n] in string 'text' is lower case.
-                    if (islower(text[a]))
-                    {
-                        text[a] = text[a] - 97;
-                        key[c] = toupper(key[c]) - 65;
-                        text[a] = (text[a] + key[c % keylen]) % 26;
-                        text[a] = text[a] + 97;
-                        printf("%c", text[a]);
-                        c++;
-                    }
+        // checks if the char[n] in string 'text' is alphabetical.
+        if (isalpha(text[a]))
+        {
+            // checks if the char[n] in string 'text' is lower case.
+            if (islower(text[a]))
+            {
+                text[a] = text[a] - 97;
+                key[c] = toupper(key[c]) - 65;
+                text[a] = (text[a] + key[c % keylen]) % 26;
+                text[a] = text[a] + 97;
+                printf("%c", text[a]);
+                c++;
+            }
 
-                    // checks if the char[n] in string 'text' is upper case.
-                    else if (isupper(text[a]))
-                    {
-                        text[a] = text[a] - 65;
-                        key[c] = toupper(key[c]) - 65;
-                        text[a] = (text[a] + key[c % keylen]) % 26;
-                        text[a] = text[a] + 65;
-                        printf("%c", text[a]);
-                        c++;
-                    }
-                }
+            // checks if the char[n] in string 'text' is upper case.
+            else if (isupper(text[a]))
+            {
+                text[a] = text[a] - 65;
+                key[c] = toupper(key[c]) - 65;
+                text[a] = (text[a] + key[c % keylen]) % 26;
+                text[a] = text[a] + 65;
+                printf("%c", text[a]);
+                c++;
+            }
+        }
 
-                // breaks out of the loop in order not to print the '\0' tag that represents the end of a string in an array.
-                else if (text[a] == b)
-                {
-                    break;
-                }
+        // breaks out of the loop in order not to print the '\0' tag that represents the end of a string in an array.
+        else if (text[a] == b)
+        {
+            break;
+        }
 
-                // in order not to encypher spaces.
-                else
-                {
-                    key[c] = key[c];
-                    printf("%c", text[a]);
-                }
+        // in order not to encypher spaces.
+        else
+        {
+            key[c] = key[c];
+            printf("%c", text[a]);
+        }
     }
 
     // prints a new line before main ends.
