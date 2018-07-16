497acbf9470e8253467c422db6261bb1a3d48c79
João Marujo <jppmarujo@gmail.com>
2018-07-16 18:52:35 +0000

    Followed CS50x code styling rules.

--- pset1/cash/cash.c
+++ pset1/cash/cash.c
@@ -9,41 +9,42 @@
 
 int main(void)
 {
-int coinsNumber = 0, changeInCents, quarter = 25, dime = 10, nickel = 5, pennie = 1;
-float change;
+    int coinsNumber = 0, changeInCents, quarter = 25, dime = 10, nickel = 5, pennie = 1;
+    float change;
 
-do
-{
-    change = get_float("How much change do we owe you?: "); // ask for user input regarding the change.
-}
+    do
+    {
+        change = get_float("How much change do we owe you?: "); // ask for user input regarding the change.
+    }
 
-while(change < 0); // keep asking while the condition is true.
+    while (change < 0); // keep asking while the condition is true.
 
-changeInCents = round(change * 100); // convert dollars to cents, since all coins are in cents. Round it, because of float imprecision!
+    changeInCents = round(change * 100); // convert dollars to cents, since all coins are in cents. Round it.
 
-while(changeInCents >= quarter) // always give the biggest coin (quarter), while it is possible.
-{
-    changeInCents = changeInCents - quarter; // take a quarter from the change.
-    coinsNumber++; // add a coin to the count, in order to see how many we have to give in the end.
-}
+    while (changeInCents >= quarter) // always give the biggest coin (quarter), while it is possible.
+    {
+        changeInCents = changeInCents - quarter; // take a quarter from the change.
+        coinsNumber++; // add a coin to the count, in order to see how many we have to give in the end.
+    }
 
-while(changeInCents >= dime) // if a quarter is not possible, run this loop.
-{
-    changeInCents = changeInCents - dime;
-    coinsNumber++;
-}
+    while (changeInCents >= dime) // if a quarter is not possible, run this loop.
+    {
+        changeInCents = changeInCents - dime;
+        coinsNumber++;
+    }
 
-while(changeInCents >= nickel) // if a quarter, or a dime are not possible, run this loop.
-{
-    changeInCents = changeInCents - nickel;
-    coinsNumber++;
-}
+    while (changeInCents >= nickel) // if a quarter, or a dime are not possible, run this loop.
+    {
+        changeInCents = changeInCents - nickel;
+        coinsNumber++;
+    }
 
-while(changeInCents >= pennie) // if a quarter, or a dime, or a nickel are not possible, run this loop.
-{
-changeInCents = changeInCents - pennie;
-coinsNumber++;
-}
+    while (changeInCents >= pennie) // if a quarter, or a dime, or a nickel are not possible, run this loop.
+    {
+        changeInCents = changeInCents - pennie;
+        coinsNumber++;
+    }
 
-printf("%i\n", coinsNumber); // prints the number of least ammount of coins to give, in relation to the change.
+    printf("%i\n", coinsNumber); // prints the number of least ammount of coins to give, in relation to the change.
+    return 0;
 }
--- pset1/mario/less/mario.c
+++ pset1/mario/less/mario.c
@@ -8,35 +8,36 @@
 
 int main(void)
 {
-// declare the variable before assigning a value. On comments, assume height is 5 for better understanding.
-int height;
+    // declare the variable before assigning a value. On comments, assume height is 5 for better understanding.
+    int height;
 
-do
-{
-    // ask for the user input, regardless of the value. Assume it is 5.
-    height = get_int("Select a height for your tower: ");
-}
-// keep asking for input, if height is: 1. negative; 2. greater than 23; 3. something other than an integer.
-while (height < 0 || height > 23);
-
-// print 5 rows, while the number of rows (that start as 0), is lower than the height.
-for (int rows = 0; rows < height; rows++)
-{
-    // spaces start at 0. While spaces are lesser than 5 - 4 - 1 (on finish, 0 can't be lesser than 0), add another space.
-    for (int spaces = 0; spaces < height - rows - 1; spaces++)
+    do
     {
-        // actually print the space.
-        printf(" ");
+        // ask for the user input, regardless of the value. Assume it is 5.
+        height = get_int("Select a height for your tower: ");
     }
-    // hashes start at 0. While they are less than 4 + 2 (since rows start at 0, so not 5 + 2), add another hash.
-    for (int hashes = 0; hashes < rows + 2; hashes++)
+
+    // keep asking for input, if height is: 1. negative; 2. greater than 23; 3. something other than an integer.
+    while (height < 0 || height > 23);
+
+    // print 5 rows, while the number of rows (that start as 0), is lower than the height.
+    for (int rows = 0; rows < height; rows++)
     {
-        // actually print the hash.
-        printf("#");
+        // spaces start at 0. While spaces are lesser than 5 - 4 - 1 (on finish, 0 can't be lesser than 0), add another space.
+        for (int spaces = 0; spaces < height - rows - 1; spaces++)
+        {
+            // actually print the space.
+            printf(" ");
+        }
+
+        // hashes start at 0. While they are less than 4 + 2 (since rows start at 0, so not 5 + 2), add another hash.
+        for (int hashes = 0; hashes < rows + 2; hashes++)
+        {
+            // actually print the hash.
+            printf("#");
+        }
+
+        // each row is, naturally (since it's a row) printed on a new line.
+        printf("\n");
     }
-// each row is, naturally (since it's a row) printed on a new line.
-printf("\n");
-}
-
-
 }
